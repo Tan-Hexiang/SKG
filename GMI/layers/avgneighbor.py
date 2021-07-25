b716bbc6e25fc7025cef119b3257050909e6e090
ionicbond0825 <39156435+ionicbond0825@users.noreply.github.com>
2021-07-25 10:06:47 +0800

    Add files via upload
    互信息损失函数借鉴了GMI的损失函数，主要是将节点A的隐向量和其他节点的特征向量和领域向量进行对比学习

---
+++ GMI/layers/avgneighbor.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+from utils import process
+
+# Applies mean-pooling on neighbors
+class AvgNeighbor(nn.Module):
+    def __init__(self):
+        super(AvgNeighbor, self).__init__()
+
+    def forward(self, seq, adj_ori):
+        # 稀疏矩阵转成torch类型的矩阵
+        # adj_ori = process.sparse_mx_to_torch_sparse_tensor(adj_ori)
+        # if torch.cuda.is_available():
+        #     adj_ori = adj_ori.cuda()
+        # torch.spmm相当于矩阵乘法
+        # 这里相当于得到节点的邻居表示的和
+        return torch.unsqueeze(torch.spmm(adj_ori, torch.squeeze(seq, 0)), 0)
