15ae67bc1072dd12338473a6953e78593cb47a18
ionicbond0825 <39156435+ionicbond0825@users.noreply.github.com>
2021-08-19 19:05:30 +0800

    Add files via upload
    更新了GAT用于节点分类的代码

--- GAT/model_GAT.py
+++ GAT/model_GAT.py
@@ -10,82 +10,96 @@ Pytorch implementation: https://github.com/Diego999/pyGAT
 import torch
 import torch.nn as nn
 import dgl.function as fn
-from dgl.nn import GATConv
+from dgl.nn.pytorch import GATConv
 
 
-class GAT(nn.Module):
+class GAT_NC(nn.Module):
     def __init__(self,
                  g,
-                 num_layers,
+                 n_layers,
                  in_dim,
-                 num_hidden,
-                 num_classes,
+                 hidden_dim,
+                 out_dim,
                  heads,
                  activation,
                  feat_drop,
                  attn_drop,
                  negative_slope,
                  residual):
-        super(GAT, self).__init__()
+        super(GAT_NC, self).__init__()
         self.g = g
-        self.num_layers = num_layers
+        self.n_layers = n_layers
         self.gat_layers = nn.ModuleList()
         self.activation = activation
         # input projection (no residual)
         self.gat_layers.append(GATConv(
-            in_dim, num_hidden, heads[0],
+            in_dim, hidden_dim, heads[0],
             feat_drop, attn_drop, negative_slope, False, self.activation))
         # hidden layers
-        for l in range(1, num_layers):
-            # due to multi-head, the in_dim = num_hidden * num_heads
+        for l in range(1, n_layers):
+            # due to multi-head, the in_dim = hidden_dim * num_heads
             self.gat_layers.append(GATConv(
-                num_hidden * heads[l-1], num_hidden, heads[l],
+                hidden_dim * heads[l-1], hidden_dim, heads[l],
                 feat_drop, attn_drop, negative_slope, residual, self.activation))
-        # output projection
-        self.gat_layers.append(GATConv(
-            num_hidden * heads[-2], num_classes, heads[-1],
-            feat_drop, attn_drop, negative_slope, residual, None))
+        # hidden vector projection
+        # self.gat_layers.append(GATConv(
+        #     hidden_dim * heads[-2], hidden_dim, heads[-1],
+        #     feat_drop, attn_drop, negative_slope, residual, None))
+        # output vector projection
+        self.gat_out = GATConv(
+            hidden_dim * heads[-1], out_dim, heads[-1],
+            feat_drop, attn_drop, negative_slope, residual, None)
+        self.fc = nn.Linear(in_dim, hidden_dim, bias=False)
 
     def forward(self, inputs):
         h = inputs
-        for l in range(self.num_layers):
+        for l in range(self.n_layers):
             h = self.gat_layers[l](self.g, h).flatten(1)
         # output projection
-        logits = self.gat_layers[-1](self.g, h).mean(1)
-        return logits
+        logits = self.gat_out(self.g, h).mean(1)
+        # h = self.gat_layers[-1](self.g, h).flatten(1)
+        seq_fts = self.fc(inputs)
+        return logits, h, seq_fts
 
-class GAT_PF(nn.Module):
+class GAT_LP(nn.Module):
     def __init__(self,
                  g,
-                 num_layers,
+                 n_layers,
                  in_dim,
-                 num_hidden,
+                 hidden_dim,
                  heads,
                  activation,
                  feat_drop,
                  attn_drop,
                  negative_slope,
                  residual):
-        super(GAT_PF, self).__init__()
+        super(GAT_LP, self).__init__()
         self.g = g
-        self.num_layers = num_layers
+        self.n_layers = n_layers
         self.gat_layers = nn.ModuleList()
         self.activation = activation
         # input projection (no residual)
         self.gat_layers.append(GATConv(
-            in_dim, num_hidden, heads[0],
+            in_dim, hidden_dim, heads[0],
             feat_drop, attn_drop, negative_slope, False, self.activation, allow_zero_in_degree=True))
         # hidden layers
-        for l in range(1, num_layers):
-            # due to multi-head, the in_dim = num_hidden * num_heads
+        for l in range(1, n_layers):
+            # due to multi-head, the in_dim = hidden_dim * num_heads
             self.gat_layers.append(GATConv(
-                num_hidden * heads[l-1], num_hidden, heads[l],
+                hidden_dim * heads[l-1], hidden_dim, heads[l],
                 feat_drop, attn_drop, negative_slope, residual, self.activation, allow_zero_in_degree=True))
+        # output projection
+        # self.gat_layers.append(GATConv(
+        #     hidden_dim * heads[-2], hidden_dim, heads[-1],
+        #     feat_drop, attn_drop, negative_slope, residual, self.activation, allow_zero_in_degree=True))
+        self.fc = nn.Linear(in_dim, hidden_dim, bias=False)
 
     def forward(self, inputs):
         h = inputs
-        for l in range(self.num_layers):
+        for l in range(self.n_layers):
             h = self.gat_layers[l](self.g, h).flatten(1)
         # output projection
         adj_rec = torch.sigmoid(torch.matmul(h, h.t()))
-        return adj_rec, h
+        # 将特征进行线性变换，用来进行对比学习
+        seq_fts = self.fc(inputs)
+        return adj_rec, h, seq_fts
